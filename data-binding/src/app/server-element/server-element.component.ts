f19c89c7b706ab78eb58814d99442c5bbdc41090
kvin1087 <kaustubh87@gmail.com>
2018-02-07 20:50:52 -0600

    Adding Alias to a property element

--- data-binding/src/app/server-element/server-element.component.ts
+++ data-binding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit , Input} from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: { type: string, name: string, content: string};
+  //@Input() element: { type: string, name: string, content: string};
+
+  @Input('srvElement') element: { type: string, name: string, content: string};
 
   constructor() { }
 
