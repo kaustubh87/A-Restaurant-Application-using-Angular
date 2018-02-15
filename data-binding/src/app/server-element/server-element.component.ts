177969b7cd10b5e8fdb53dd0c302983a49ebd6c8
kvin1087 <kaustubh87@gmail.com>
2018-02-14 22:35:25 -0600

    Added imports

--- data-binding/src/app/server-element/server-element.component.ts
+++ data-binding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit , Input, ViewEncapsulation, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,22 @@ import { Component, OnInit , Input, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None
 })
-export class ServerElementComponent implements OnInit {
+// tslint:disable-next-line:one-line
+export class ServerElementComponent implements OnInit, OnChanges{
 
   //@Input() element: { type: string, name: string, content: string};
 
+  // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: { type: string, name: string, content: string};
+  @Input() name: string;
 
   constructor() {
     console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnchanges called');
+    console.log(changes);
   }
 
   ngOnInit() {
